797fa8608fe170bb3adcd2e99cffd72bd154428a
Devraj <devaraju.railsfactory@weinvest.net>
2019-05-01 18:28:59 +0530

    stack with liked list

---
+++ stack_with_linked_list/stack_with_linked_list.rb
@@ -0,0 +1,47 @@
+module LinkedList
+  class Node
+    attr_accessor :data, :link
+    def initialize(data, link)
+      @data = data
+      @link = link
+    end
+  end
+
+  class Stack
+    def initialize
+      @top = nil
+    end
+
+    def push(ele)
+      @top = Node.new(ele, @top)
+    end
+    alias_method :'<<', :push
+
+    def pop
+      raise "Stack empty" if is_empty?
+      data = @top.data
+      @top = @top.link
+      data
+    end
+
+    def top
+      unless is_empty?
+        @top.data
+      else
+        p "stack Empty"
+      end
+    end
+
+    def display
+      tmp = @top
+      while !tmp.nil?
+        p tmp.data
+        tmp = tmp.link
+      end
+    end
+
+    def is_empty?
+      @top.nil?
+    end
+  end
+end
